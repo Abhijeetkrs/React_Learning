8e52dcf682b45dad3b22cfa5c7688f06a4737b86
Abhijeet <abhijeetsharma2003@gmail.com>
2025-02-06 12:27:30 +0530

    2nd lecture

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Hello React (create-react-app) heavy,bulky in size</h1>
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Hello React+Vite (this is lightWeight process)</h1>
+  )
+}
+
+export default App
